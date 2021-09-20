a99c34e36a367d2061c5a5951f9155506382b626
elisson <elissonsaldanha19@gmail.com>
2021-09-20 10:49:43 -0300

    fix to bug

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 //verirficar se é um numero binario
 function validaty(num){
    const verificaty = /2|3|4|5|6|7|8|9/
-   return verificaty.test(num)
+   const regExpreLetter = /[a-z]/i
+   return verificaty.test(num) || regExpreLetter.test(num)
 }
 
 
@@ -35,11 +36,12 @@ function Innertext(text){
 
 
 function start(num){
-    let binaryNumber = invertArray(num)
-    if(validaty(binaryNumber)){
+    if(validaty(num)){
         Innertext('somente numeros binarios')
         return
     }
+    let binaryNumber = invertArray(num)
+  
     binaryNumber = binToNumber(binaryNumber)
     Innertext(`O numero é ${binaryNumber}`)
 }
@@ -55,8 +57,8 @@ document.getElementById('become').addEventListener('click', ()=>{
 
  // logica
 
+ // verificar se na palavra tem os numero 2,3,4,5,6,7,8,9 ou letras com expressões regulares
  // inverter o numero
- // verificar se na palavra tem os numero 2,3,4,5,6,7,8,9 com expressões regulares
  // Se tiver enviar uma mesagem de erro
  // Se não tiver transforma para numero decimal
  // enviar para a tela 
